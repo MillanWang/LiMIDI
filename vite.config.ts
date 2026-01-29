18e9a18bc7a24db4545841f29817820b05b75418
Millan Wang <real.millan.wang@gmail.com>
2026-01-29 01:20:17 -0500

    Update vite config to updated repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/limidi/' : '/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/LiMIDI/' : '/',
 })
